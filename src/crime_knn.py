372ae033778e421cc36201e634816699ab78cae0
Gustavo H. Torres <gus7ht@gmail.com>
2026-04-25 18:31:47 +0200

    .

--- src/crime_knn.py
+++ src/crime_knn.py
@@ -3,10 +3,10 @@ import numpy as np
 import sys, os
 import pyinputplus as pyip
 
-sys.path.insert(0, os.path.abspath('..'))   # so max_heap.py is importable
+#sys.path.insert(0, os.path.abspath('..'))   # so max_heap.py is importable
 from max_heap import MaxHeap, euclidean_distance
 
-sys.path.insert(0, os.path.abspath('..'))   # so max_heap.py is importable
+#sys.path.insert(0, os.path.abspath('..'))   # so max_heap.py is importable
 from src.preprocess_data import preprocess_data
 
 # --- Primary Type filter:
